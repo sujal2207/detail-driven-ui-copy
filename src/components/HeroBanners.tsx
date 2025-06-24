8b64fcc98902c24c2510d82d2197c111500ad8c6
KaushalSojitra1911 <sujalpaghadal17@gmail.com>
2025-06-24 15:35:15 +0530

    small changes

--- src/components/HeroBanners.tsx
+++ src/components/HeroBanners.tsx
@@ -6,12 +6,12 @@ const HeroBanners = () => {
         <div className={'flex justify-space-between align-middle'}>
           <div className={'mr-2'}>
             <img
-                src="https://assets.staples-static.com/sbd/cre/products/home-banners/20250622/2360402/updates_baked/images/2360402_wk4_2across_paperdeals_46_41_1332x560.jpg?imWidth=1416"
+                src="https://assets.staples-static.com/sbd/cre/products/home-banners/20250622/2360402/updates_baked/images/2360402_wk4_2across_grad2_1332x560.jpg?imWidth=1416"
                 alt="" className={'h-[300px]'}/>
           </div>
           <div>
             <img
-                src="https://assets.staples-static.com/sbd/cre/products/home-banners/20250622/2360402/updates_baked/images/2360402_wk4_2across_kcups_1332x560.jpg?imWidth=1416"
+                src="https://assets.staples-static.com/sbd/cre/products/home-banners/20250622/2360402/updates_baked/images/2360402_wk4_2across_bts_1332x560.jpg?imWidth=1416"
                 alt="" className={'h-[300px]'}/>
           </div>
         </div>
